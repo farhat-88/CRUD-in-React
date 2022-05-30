53c8dbaeac63c2a9ef89d29603056ad0f988183c
farhat-88 <farhat>
2022-05-30 12:22:46 +0500

    CRUD

---
+++ src/components/array.js
@@ -0,0 +1,20 @@
+// Javascript object named array with 3 key-values
+const array = [
+	{
+		id:'1',
+		Name:'Ali',
+		Age:'23'
+	},
+	{
+		id:'2',
+		Name:'Shah',
+		Age:'22'
+	},
+	{
+		id:'3',
+		Name:'Farhan',
+		Age:'23'
+	}
+]
+
+export default array
